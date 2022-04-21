e3f914e0cb85891eb5d1a990c763535d11cf25cb
yangfangming <451493485@qq.com>
2022-04-21 14:16:50 +0800

    -

---
+++ BluetoothStubOnIOS/BluetoothStubOnIOS/FMEventObject.h
@@ -0,0 +1,17 @@
+//
+//  FMEventObject.h
+//  BluetoothStubOnIOS
+//
+//  Created by yfm on 2022/4/21.
+//  Copyright © 2022 刘彦玮. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FMEventObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BluetoothStubOnIOS/BluetoothStubOnIOS/FMEventObject.m
@@ -0,0 +1,13 @@
+//
+//  FMEventObject.m
+//  BluetoothStubOnIOS
+//
+//  Created by yfm on 2022/4/21.
+//  Copyright © 2022 刘彦玮. All rights reserved.
+//
+
+#import "FMEventObject.h"
+
+@implementation FMEventObject
+
+@end
